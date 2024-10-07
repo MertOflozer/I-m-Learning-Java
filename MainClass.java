1005f29992bfa7efebc6f6a93312d3c1896bc758
Mert Oflozer <45922510+MertOflozer@users.noreply.github.com>
2024-10-08 01:21:32 +0300

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,28 @@
+// JVM, JRE, JDK Concepts
+
+// JVM // Java Virtual Machine
+// JRE // Java Runtime Environment
+// JDK // Java Development Kit
+
+// JDK = Libraries + JRE
+// JRE = Libraries + JVM
+// JVM = ..................
+
+
+// Main Method Concept
+
+package javaogrenmece;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	
+		// 1. code
+		// 2. code
+		// 3. code
+		// ......
+		// ......
+		
+	}
+	
+}
